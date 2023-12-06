2ad3a0b42bb0dc394d3a5c5552626ced7d5caed0
yuradev16 <201239@ids.upchiapas.edu.mx>
2023-12-05 21:31:40 -0600

    users commit

---
+++ IdentityAccessManagment/src/user/infraestructure/routers/userRouter.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import { loginUserController, resgisterUserController, updatePasswordController, updateUserByIdController,usergetController } from "../dependencies";
+import { validateToken } from "../../../helpers/veryfyToken";
+
+
+
+export const userRoutes = express.Router();
+userRoutes.get('/:uuid',usergetController.getRestaurantId.bind(usergetController));
+userRoutes.post('/',resgisterUserController.run.bind(resgisterUserController)) 
+
+userRoutes.post('/login',loginUserController.run.bind(loginUserController))
+
+userRoutes.put('/id',validateToken,updateUserByIdController.run.bind(updateUserByIdController))
+
+userRoutes.put('/restar_password',updatePasswordController.run.bind(updatePasswordController))
+
+
+
+
+
+
+
+
+
+
